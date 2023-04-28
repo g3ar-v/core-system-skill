cec6e1c7f60f93b980e38d80bc9b4f6cea9360e8
victor <vfranktor@gmail.com>
2023-04-28 14:13:44 +0100

    feat(__init__.py): add skeleton code for shutting and rebooting host

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
-from core.skills.core import MycroftSkill, intent_file_handler
-from core.util.log import LOG
-from os.path import join, expanduser, abspath
 import os
+import subprocess
 import sys
 import time
+from os.path import join
+
+from core.skills.core import MycroftSkill
+from core.util.log import LOG
 
 
 class LinuxSkill(MycroftSkill):
@@ -12,15 +14,16 @@ class LinuxSkill(MycroftSkill):
 
     def initialize(self):
 
-        mycroft_core_path = os.path.join(os.path.dirname(sys.modules['core'].__file__), '..')
+        mycroft_core_path = os.path.join(os.path.dirname(sys.modules['core'].__file__),
+                                         '..')
         self.mycroft_core_path = os.path.abspath(mycroft_core_path)
 
         print("mycroft-core path:", mycroft_core_path)
 
-        self.add_event('system.shutdown', self.handler_system_shutdown)
-        self.add_event('system.reboot', self.handler_system_reboot)
+        self.add_event('system.shutdown', self.handler_core_shutdown)
+        self.add_event('system.reboot', self.handler_core_reboot)
 
-    def handler_system_shutdown(self, message):
+    def handler_core_shutdown(self, message):
         """
         Shuts down mycroft modules not the OS
         """
@@ -30,7 +33,7 @@ class LinuxSkill(MycroftSkill):
         LOG.info(path)
         os.system(path)
 
-    def handler_system_reboot(self, message):
+    def handler_core_reboot(self, message):
         """
         Restart mycroft modules not the OS
         """
@@ -39,6 +42,13 @@ class LinuxSkill(MycroftSkill):
         path = join(self.mycroft_core_path, 'start-mycroft.sh all restart')
         LOG.info(path)
         os.system(path)
+
+    def handle_system_reboot(self, _):
+        self.speak_dialog("rebooting", wait=True)
+        subprocess.call(["/usr/bin/systemctl", "reboot"])
+
+    def handle_system_shutdown(self, _):
+        subprocess.call(["/usr/bin/systemctl", "poweroff"])
 
     def stop(self):
         pass
